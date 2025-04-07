3f33a1836fff3ce8b11a042e9d5fa78ef90cfa39
NiveditaPatle <nivepatle@gmail.com>
2025-04-07 09:04:32 +0530

    style que

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { checkHeading, replaceHeadingStars } from '../helper';
 
-const Answers = ({ans, totalResult, index}) => {
+const Answers = ({ans, totalResult, index, type}) => {
   
   const [heading, setHeading] = useState(false);
   const [answer, setAnswer] = useState(ans);
@@ -22,7 +22,7 @@ const Answers = ({ans, totalResult, index}) => {
     <>
       {
         index==0 && totalResult>1 ?<span className=' pt-2 text-xl block text-zinc-200'>{answer}</span> :
-        heading? <span className=' pt-2 text-lg block text-white'>{answer}</span> : <span className='pl-5'>{answer}</span>
+        heading? <span className=' pt-2 text-lg block text-white'>{answer}</span> : <span className={type == 'q' ? 'pl-1' : 'pl-5'}>{answer}</span>
       }
     </>
   )
