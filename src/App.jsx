07a4775dc75b6a08cf7485e5a6451985e1d02475
NiveditaPatle <nivepatle@gmail.com>
2025-04-07 11:20:09 +0530

    delete history

--- src/App.jsx
+++ src/App.jsx
@@ -44,14 +44,23 @@ function App() {
   console.log(recentHistory);
   
 
+  const clearHistory = () => {
+    localStorage.clear();
+    setRecentHistory([]);
+  }
 
   return (
    <div className="grid grid-cols-5 h-screen text-center">
-    <div className=" bg-zinc-800 col-span-1">
-      <ul>
+    <div className=" bg-zinc-800 pt-3 col-span-1">
+    <h1 className="text-xl text-white flex justify-center">
+    <span>Resent Search </span>
+    <button className=" cursor-pointer" onClick={clearHistory}>
+    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#D9D9D9"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+    </button></h1>
+      <ul className=" text-left overflow-auto mt-3">
         {
           recentHistory && recentHistory.map((item) => (
-            <li>{item}</li>
+            <li className="p-1 pl-5 px-4 truncate text-zinc-400 cursor-pointer hover:bg-zinc-700 hover:text-zinc-200">{item}</li>
           ))
         }
       </ul>
